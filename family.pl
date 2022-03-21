6532e085e323f3eb52189f2b8539b14ffe2dfc31
J. Pryce <71295464+kyuukestu@users.noreply.github.com>
2022-03-21 00:36:27 -0500

    Create family.pl

---
+++ family.pl
@@ -0,0 +1,7 @@
+Write a PROLOG program that implements a family database for your family. Save it as an ordinary 
+text file named family.pl.
+Your program should implement the following facts for your immediate family, grandparents, and 
+great-grandparents.
+parent_of(X,Y).
+male(X).
+female(Y).
