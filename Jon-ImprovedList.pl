48504ad9b75c87756098f241e541c2d5dd7ca217
J. Pryce <71295464+kyuukestu@users.noreply.github.com>
2022-05-01 18:21:27 -0500

    Update Jon-ImprovedList.pl

--- Jon-ImprovedList.pl
+++ Jon-ImprovedList.pl
@@ -5,8 +5,8 @@
 %  family([mom,dad],[[child1,female],[child2,male],[child3,male]]).
 
 family([tile, banca], [[leonie, female]]).
-family([leonie, badlwin], [[jeffine, female], [charlene, female], [suzette, female], [phil, male]]).
-family([jeffine, williamsr], [[williamjr, male]]).
+family([leonie, baldwin], [[jefferine, female], [charlene, female], [suzette, female], [phil, male]]).
+family([jefferine, williamsr], [[williamjr, male]]).
 family([charlene, darlston], [[jon, male]]).
 family([lacey, phil], [[zxvana, female], [zxrai, female]]).
 
@@ -28,6 +28,11 @@ female(zxrai).
 female(lacey).
 female(tile).
 
+on(Item, [Item|_]).
+size([], 0).
+size([_|T], N) :- size(T, N1), N1 is N + 1.
+
+
 father_of(F, C) :- family([_, F], Cs), member([C|_], Cs).
 mother_of(M, C) :- family([M, _], Cs),  member([C|_], Cs).
 parent_of(P, C) :- father_of(P, C) ; mother_of(P, C).
@@ -35,4 +40,10 @@ sibling_of(S1, S2) :- parent_of(P, S1), parent_of(P, S2).
 brother_of(B, S) :- sibling_of(B, S) , male(B).
 sister_of(S, Si) :- sibling_of(S, Si), female(Si).
 oldest_child_of(C, P) :- parent_of(P, C), family(_, Cs), on([C|_], Cs).
-oldest_daughter_of(D, P) :- parent_of(P, D), 
+number_of_children(P, N) :- (family([_, P], Cs) ; family([P, _], Cs)), size(Cs, N).
+
+
+
+oldest_son_of(S, P):- parent_of(P, S), family(_, Ss), male(S).
+oldest_daughter_of(D, P) :- parent_of(P, D), family(_, Ds), female(D).
+
