b08f69eac9bca0d133e6888db3ea729c33c7b538
Roshaun556 <80638438+Roshaun556@users.noreply.github.com>
2022-05-01 21:15:27 -0400

    Update Roshaunfamily.pl

--- Roshaunfamily.pl
+++ Roshaunfamily.pl
@@ -1,7 +1,10 @@
-Write a PROLOG program that implements a family database for your family. Save it as an ordinary 
-text file named family.pl.
-Your program should implement the following facts for your immediate family, grandparents, and 
-great-grandparents.
+%Write a PROLOG program that implements a family database for your family. Save it as an ordinary 
+%text file named family.pl.
+%Your program should implement the following facts for your immediate family, grandparents, and 
+%great-grandparents.
+
+%Gender Declarations
+
 female(Y).
 female(tash).
 female(kay).
@@ -19,6 +22,8 @@ male(cgee).
 male(glan).
 male(cgee).
 male(hodgson).
+
+%Declaration of Parents
 parent_of(P,C) :- father_of(P,C).
 parent_of(P,C) :- mother_of(P,C).
 parent_of(kay,anbal).
@@ -33,13 +38,11 @@ parent_of(anbal,glan).
 parent_of(leigh,hodgson).
 parent_of(west,cgee).
 parent_of(west,catherine).
+
+%Predicates
 sibling(X,Y):-parent_of(Z,X),parent_of(Z,Y),not(X=Y).
 mother_of(X,Y):-parent_of(X,Y), female(X).
 father_of(X,Y):-parent_of(X,Y), male(X).
-father_of(D,C) :- family([_,D],[Ds,Ss]),
- (member(C,Ds);
- member(C,Ss)).
-
 daughter_of(X,Y):- parent_of(Y,X),female(X). 
 sister(X,Y):-sibling(X,Y),female(X).
 son_of(X,Y):- parent_of(Y,X),male(X). 
