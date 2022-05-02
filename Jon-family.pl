770aaee552472caea3d6a7f8959eeb915b229510
J. Pryce <71295464+kyuukestu@users.noreply.github.com>
2022-05-01 21:48:51 -0500

    Update Jon-family.pl

--- Jon-family.pl
+++ Jon-family.pl
@@ -46,15 +46,21 @@ parent_of(baldwin, jefferine).
 parent_of(tile, leonie).
 parent_of(banca, leonie).
 
-% Predicates
+% Parentage Predicates
 father_of(X, Y) :- parent_of(X, Y), male(X).
 mother_of(X, Y) :- parent_of(X, Y), female(X).
-daughter_of(X, Y):- parent_of(Y, X),female(X). 
-son_of(X, Y):- parent_of(Y, X), male(X). 
-sister_of(X, Y):- sibling(X, Y), female(X).
-sibling_of(X,Y):- parent_of(Z, X), parent_of(Z, Y), not(X = Y).
-brother(X, Y):- sibling(X, Y), male(X).
 grandmother(X, Z):- parent_of(X, Y), parent_of(Y, Z), female(X).
 grandfather(X, Z):- parent_of(X, Y), parent_of(Y, Z), male(X).
 ancestor_of(X, Y):- parent_of(X, Y).
 ancestor_of(X, Y):- parent_of(X, Z), ancestor_of(Z, Y).
+
+% Sibling predicates
+sister_of(X, Y):- sibling(X, Y), female(X).
+sibling_of(X,Y):- parent_of(Z, X), parent_of(Z, Y), not(X = Y).
+brother(X, Y):- sibling(X, Y), male(X).
+
+% Child Predicates
+daughter_of(X, Y):- parent_of(Y, X),female(X). 
+son_of(X, Y):- parent_of(Y, X), male(X). 
+
+
