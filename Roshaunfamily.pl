277ee72eee15fd4aaa611c48b4c6db530ed03901
Roshaun556 <80638438+Roshaun556@users.noreply.github.com>
2022-05-02 00:44:49 -0400

    Update Roshaunfamily.pl

--- Roshaunfamily.pl
+++ Roshaunfamily.pl
@@ -5,46 +5,59 @@
 
 %Gender Declarations
 
-female(Y).
-female(tash).
-female(kay).
-female(anbal).
-female(jean).
-female(caroline).
-female(catherine).
-male(X).
-male(M) :- family([_,M],_).
-male(M) :- family(_,[_,Ss]), member(M,Ss).
-male(roshaun).
-male(leigh).
-male(west).
+%Gender Declarations
+
+male(_X).
 male(cgee).
+male(smug).
+male(james).
 male(glan).
 male(cgee).
 male(hodgson).
+male(leigh).
+male(west).
+male(roshaun).
+
+female(_Y).
+female(cynthia).
+female(ruth).
+female(jean).
+female(caroline).
+female(catherine).
+female(anbal).
+female(tash).
+female(kay).
+
+
+
+
 
 %Declaration of Parents
-parent_of(P,C) :- father_of(P,C).
-parent_of(P,C) :- mother_of(P,C).
-parent_of(kay,anbal).
-parent_of(kay,leigh).
-parent_of(tash,anbal).
-parent_of(tash,west).
-parent_of(roshaun,anbal).
-parent_of(roshaun,leigh).
+
+parent_of(jean,smug).
+parent_of(jean,cynthia).
+parent_of(glan,james).
+parent_of(glan,ruth).
 parent_of(anbal,caroline).
 parent_of(leigh,jean).
 parent_of(anbal,glan).
 parent_of(leigh,hodgson).
 parent_of(west,cgee).
 parent_of(west,catherine).
+parent_of(kay,anbal).
+parent_of(kay,leigh).
+parent_of(tash,anbal).
+parent_of(tash,west).
+parent_of(roshaun,anbal).
+parent_of(roshaun,leigh).
+ 
 
 %Predicates
-sibling(X,Y):-parent_of(Z,X),parent_of(Z,Y),not(X=Y).
-mother_of(X,Y):-parent_of(X,Y), female(X).
 father_of(X,Y):-parent_of(X,Y), male(X).
-daughter_of(X,Y):- parent_of(Y,X),female(X). 
+mother_of(X,Y):-parent_of(X,Y), female(X).
+sibling(X,Y):-parent_of(Z,X),parent_of(Z,Y),not(X = Y).
 sister(X,Y):-sibling(X,Y),female(X).
+daughter_of(X,Y):- parent_of(Y,X),female(X). 
 son_of(X,Y):- parent_of(Y,X),male(X). 
 grandmother(X,Z):- parent_of(X,Y),parent_of(Y,Z),female(X).
 grandfather(X,Z):- parent_of(X,Y),parent_of(Y,Z),male(X).
